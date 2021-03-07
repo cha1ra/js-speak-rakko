75be9976c2f8dfd0c98bfff259bb89d2e46a3125
cha1ra <cha1ra.garnet@gmail.com>
2021-03-07 18:17:03 +0900

    feat: 口パクラッコ画像が滑らかになるように修正

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('text')
+const recognition = new webkitSpeechRecognition()
+recognition.lang = 'ja-JP'
+
+recognition.onsoundstart = () => {
+  console.log('音声開始')
+}
+
+recognition.onsoundend = () => {
+  console.log('音声終了！')
+}
+
+recognition.onend = () => {
+  recognition.start()
+}
+
+recognition.start()
